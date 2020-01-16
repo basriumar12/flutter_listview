859c5fabc447c9fe6c0b35c6e7fea79a6a5d0606
basri umar <basriumar12@gmail.com>
2020-01-16 13:52:34 +0700

    json first

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "Listview App",
-    home: new Home(),
+    home: new Home(data: new List<String>.generate(300, (panjangData) => "data ke $panjangData",),),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
 class Home extends StatelessWidget {
+  final List <String> data;
+  Home ({this.data});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -16,40 +20,15 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green,
         title: new Text("Listview App"),
       ),
-      body: new ListView(
-        padding: new EdgeInsets.all(20.0),
-        children: <Widget>[
-          new CustomListview(
-            image:
-                "https://www.its.ac.id/news/wp-content/uploads/sites/2/2019/11/images.jpg",
-            judul: "Pohon Beringin",
-          ),
-          new CustomListview(
-            image:
-                "https://i2.wp.com/rumahtanaman.com/wp-content/uploads/2018/01/Bidara.jpg",
-            judul: "Pohon Bidara",
-          ),
-          new CustomListview(
-            image:
-                "https://3.bp.blogspot.com/-COtEgMjhBKo/XE-_1R_DaaI/AAAAAAAADTk/Jf-jq9CryLUfgPXHqDB9Pm3aOjIhM_ptACLcBGAs/s1600/Pohon-Rambutan-02.jpg",
-            judul: "Pohon Rambutan",
-          ),
-          new CustomListview(
-            image:
-                "https://www.its.ac.id/news/wp-content/uploads/sites/2/2019/11/images.jpg",
-            judul: "Pohon Beringin",
-          ),
-          new CustomListview(
-            image:
-                "https://www.its.ac.id/news/wp-content/uploads/sites/2/2019/11/images.jpg",
-            judul: "Pohon Beringin",
-          ),
-          new CustomListview(
-            image:
-                "https://www.its.ac.id/news/wp-content/uploads/sites/2/2019/11/images.jpg",
-            judul: "Pohon Beringin",
-          ),
-        ],
+      body: new Container(
+        child: new ListView.builder(itemCount : data.length,
+        itemBuilder: (context, index){
+          return new ListTile(
+            leading: new Icon(Icons.image),
+            title: new Text("${data[index]}"),
+          );
+        }
+        ),
       ),
     );
   }
